f0aae7f01b3bf6e28564a08730553bb028b6888f
Liliia Galkina <vvlili@yandex.ru>
2025-09-11 15:21:50 +0300

    настрока cors

--- Server/services/server.ts
+++ Server/services/server.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import cors from "cors";
+import path from "path";
 
 const host = process.env.LOCAL_PATH;
 const port = Number(process.env.LOCAL_PORT);
@@ -11,8 +12,17 @@ export function initServer(): Express {
   app.use(jsonMiddleware);
 
   app.use(cors());
+
+  // Обслуживание статических файлов
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
   app.get("/api/data", (req, res) => {
     res.json({ message: "CORS is enabled for all origins!" });
+  });
+
+  // Для SPA - отдаем index.html для всех остальных маршрутов
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 
   app.listen(port, host, () => {
